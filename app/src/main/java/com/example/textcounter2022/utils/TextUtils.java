5fd834f319e52c1693b03a87bcbb2ff34fba787f
Nemiga <pvencius@gmail.com>
2022-03-30 23:19:08 +0300

    Pridedu unit testus

--- app/src/main/java/com/example/textcounter2022/utils/TextUtils.java
+++ app/src/main/java/com/example/textcounter2022/utils/TextUtils.java
@@ -1,7 +1,12 @@
 package com.example.textcounter2022.utils;
 
 public class TextUtils {
-    public static int getCharsCount(String input) {
-        return input.length();
+    public static int getCharsCount(String givenString) {
+        return givenString.length();
+    }
+
+    public static int getWordsCount(String givenString) {
+        String words = givenString.trim();
+        return words.split("\\s+").length;
     }
 }
--- app/src/test/java/com/example/textcounter2022/ExampleUnitTest.java
+++ app/src/test/java/com/example/textcounter2022/ExampleUnitTest.java
@@ -4,14 +4,92 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import com.example.textcounter2022.utils.TextUtils;
+
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getCharsCount_Given_emptyString_Result_Zero() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue =  TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_SpacesString_Result_Four() {
+        String givenString = "    ";
+        int expectedValue = 4;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_NormalString_Result_eight() {
+        String givenString = "Test job";
+        int expectedValue = 8;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_TenCharsString_Result_Ten() {
+        String givenString = "0123456789";
+        int expectedValue = 10;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_SymbolString_Result_Five() {
+        String givenString = "/<->.";
+        int expectedValue = 5;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test(expected = NullPointerException.class)
+    public void getCharsCount_Given_NullString_Result_Exception() {
+        String givenString = null;
+        int actualValue = TextUtils.getCharsCount(givenString);
+    }
+    @Test
+    public void getWordsCount_Given_EmptyString_Result_Zero() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_Given_NormalString_Result_Eight() {
+        String givenString = "Test job";
+        int expectedValue = 2;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getWordsCount_Given_NumberString_Result_Ten() {
+        String givenString = "0123456789";
+        int expectedValue = 1;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getWordsCount_Given_SymbolString_Result_Five() {
+        String givenString = "/<->.";
+        int expectedValue = 1;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void getWordsCount_Given_NullString_Result_Exception() {
+        String givenString = null;
+        int actualValue = TextUtils.getWordsCount(givenString);
     }
 }
